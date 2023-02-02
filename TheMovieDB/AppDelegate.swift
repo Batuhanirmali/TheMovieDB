fa35fe1131f5ea5c6593dfb6930fbc30a01d7696
Talha Batuhan Irmalı <batuqwert1@gmail.com>
2023-02-02 17:05:06 +0300

    - Create Project from Scratch - Deleting SceneDelegate - Removing main.storyboard file - Grouping project files and folders based on MVVM

---
+++ TheMovieDB/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  TheMovieDB
+//
+//  Created by Talha Batuhan Irmalı on 2.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+
+        return true
+    }
+    
+}
+
---
+++ TheMovieDB/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  TheMovieDB
+//
+//  Created by Talha Batuhan Irmalı on 2.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Main View"
+        self.view.backgroundColor = .red
+    }
+
+
+}
