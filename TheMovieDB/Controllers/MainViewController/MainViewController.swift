a42e99684a89a6418ac7bd9ee12aac642932aa83
Talha Batuhan Irmalı <batuqwert1@gmail.com>
2023-02-05 16:44:25 +0300

    - MVVM is - Creating Observable Class for binding - Pass data from view model to views and controllers

--- TheMovieDB/Controllers/MainViewController/MainViewController.swift
+++ TheMovieDB/Controllers/MainViewController/MainViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var viewModel: MainViewModel = MainViewModel()
+    var cellDataSource: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
+        bindViewModel()
     }
     
-    func configView() {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.getData()
+    }
+    
+    func configView() {
         self.title = "Main View"
         self.view.backgroundColor = .blue
         setupTableView()
     }
-
-    override func viewDidAppear(_ animated: Bool) {
-        viewModel.getData()
+    func bindViewModel() {
+        viewModel.isLoading.bind { [weak self] isLoading in
+            guard let self = self, let isLoading = isLoading else {
+                return
+            }
+            DispatchQueue.main.async {
+                if isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }
+        viewModel.cellDataSource.bind {[weak self] movies in
+            guard let self = self, let movies = movies else {
+                return
+            }
+            self.cellDataSource = movies
+            self.reloadTableView()
+        }
     }
 }
